96b1d839cac7a484936f51178293311ac24592b9
Jose Victor <josevictorferreira@outlook.com>
2022-03-14 23:49:39 -0300

    updated file references

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 # Add this script to your wm startup file.
 
-DIR="$HOME/.config/polybar/cuts"
+DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -16,7 +16,7 @@ echo "Entrou aqui!"
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    echom $m
+    echo $m
     MONITOR=$m polybar -q top -c "$DIR"/config.ini &
     MONITOR=$m polybar -q bottom -c "$DIR"/config.ini &
   done
@@ -25,6 +25,6 @@ else
   polybar -q bottom -c "$DIR"/config.ini &
 fi
 
-ln -s /tmp/polybar_mqueue.$! /tmp/ipc-bottom
+ln -rs /tmp/polybar_mqueue.$! /tmp/ipc-bottom
 
 echo message >/tmp/ipc-bottom

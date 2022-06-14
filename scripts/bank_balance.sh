7e2733f9e9068f3f264c54aa0cc37464c1f64c5c
Jose Victor <josevictorferreira@outlook.com>
2022-06-14 20:24:52 -0300

    added bank balance script

---
+++ scripts/bank_balance.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# MUST DECLARE THE FOLLOWING VARIABLES IN THE bank_secrets.sh FILE:
+# BASE_URL, CLIENT_ID, CLIENT_SECRET
+# PUT THE CLIENT.CRT AND CLIENT.KEY INSIDE THE SCRIPTS FOLDER
+
+source "$HOME/.config/polybar/scripts/bank_secrets.sh"
+
+CRT_FILE_PATH="$HOME/.config/polybar/scripts/client.crt"
+KEY_FILE_PATH="$HOME/.config/polybar/scripts/client.key"
+
+get_token () {
+  curl -s -X POST "$BASE_URL/oauth/v2/token" -H "Content-Type: application/x-www-form-urlencoded" --cert "$CRT_FILE_PATH" --key "$KEY_FILE_PATH" --data "client_id=$CLIENT_ID" --data "client_secret=$CLIENT_SECRET" --data "grant_type=client_credentials" --data "scope=extrato.read" | jq -r '.access_token'
+}
+
+get_account_balance () {
+  token="$1"
+  balance=$(curl -s "$BASE_URL/banking/v2/saldo" -H "Authorization: Bearer $token" --cert "$CRT_FILE_PATH" --key "$KEY_FILE_PATH" | jq -r '.disponivel')
+  echo "R$ $balance"
+}
+
+main () {
+  token=$(get_token)
+  get_account_balance "$token"
+}
+
+main "$@"
